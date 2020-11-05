cde802896101261d6ad057bc1fceef787fb8341d
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro-2.local>
2020-11-05 06:28:59 -0800

    Implemented pagination on the home page

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,17 @@ const Pet = require('../models/pet');
 module.exports = (app) => {
 
   /* GET home page. */
-  app.get('/', (req, res) => {
-    Pet.find().exec((err, pets) => {
-      res.render('pets-index', { pets: pets });    
+  app.get('/', (req, res) =>
+    const page = req.query.page || 1
+
+    Pet.paginate({}, {page: page}).then((results) => {
+      res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });
     });
   });
-}
+
+
+// result.docs // the array of records on the current page
+// result.total // the total number of records
+// result.limit // the limit
+// result.page // the current page
+// result.pages // the total number of pages
