8bbca403dc10e7c7bce1f085a0bf5fb568dc7657
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro-2.local>
2020-12-06 14:21:06 -0800

    last commit

--- test/test-pets.js
+++ test/test-pets.js
@@ -46,6 +46,18 @@ describe('Pets', ()  => {
           done();
         });
   });
+  //TEST API
+  it('should list ALL pets on /pets GET', function(done) {
+    chai.request(server)
+        .get('/')
+        .set('content-type', 'application/json')
+        .end(function(err, res){
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          done();
+        });
+  });
 
   // TEST CREATE
   it('should create a SINGLE pet on /pets POST', (done) => {
